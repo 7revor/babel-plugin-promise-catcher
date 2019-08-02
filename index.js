71067d19c6e3a019429a58fee7db9fffcec2bd44
7revor <7071242@qq.com>
2019-08-02 11:19:19 +0800

    修复已知错误，路径筛选逻辑优化

--- index.js
+++ index.js
@@ -109,50 +109,57 @@ const funcVisitor = {
   }
 }
 
-
 module.exports = function () {
   return {
     pre(file){
-      fileName = nodePath.basename(file.opts.filename);
-      functionCatch = this.opts.functionCatch;
-      promiseCatch = this.opts.promiseCatch !== false;
-      reportInfo = this.opts.info||{}; // error info
-      const ignoreFiles = this.opts.ignoreFiles||[];
+      fileName = nodePath.basename(file.opts.filename); // 当前文件名
+      functionCatch = this.opts.functionCatch; // 是否捕获方法
+      promiseCatch = this.opts.promiseCatch !== false; // 是否捕获promise
+      reportInfo = this.opts.info||{}; // 上报信息
+      const ignoreFiles = this.opts.ignoreFiles||[]; // 忽略文件
       const functionDirs = this.opts.functionDirs||'all';
       const promiseDirs = this.opts.promiseDirs||'all';
-
-      //  step1.ignore files
+      let catchType = ''
+      //  step1.忽略文件
       if(!Array.isArray(ignoreFiles)) throw new Error('ignoreFiles must be Array<string>');
       for(let name of ignoreFiles){
         if(name === fileName) {
-          console.log('*********************************try-catching ignore file : ' + fileName);
+          console.log('\n');
+          console.log('\033[31m try-catching ignore file : \033[0m','\033[33m '+fileName+' \033[0m');
           return;
         }
       }
-      if(functionDirs!=='all'){ // function directories
-        functionCatch = true;
+      if(functionDirs!=='all'){ // 指定方法目录
+        functionCatch = false;
         if(!Array.isArray(functionDirs)) throw new Error('functionDirs must be Array<string>');
         if(!functionDirs.length) functionCatch = false;
         for(let dir of functionDirs){
-          if(!file.opts.filename.includes(dir)) return;
+          if(file.opts.filename.includes(dir)){
+            functionCatch = true;
+            catchType = 'Function '
+            break;
+          };
         }
       }
-      if(promiseDirs!=='all'){ //promise directories
-        promiseCatch = true;
+      if(promiseDirs!=='all'){ //指定promise目录
+        promiseCatch = false;
         if(!Array.isArray(promiseDirs)) throw new Error('promiseDirs must be Array<string>');
         if(!promiseDirs.length) promiseCatch = false;
         for(let dir of promiseDirs){
-          if(!file.opts.filename.includes(dir)) return;
+          if(file.opts.filename.includes(dir)){
+            promiseCatch = true;
+            catchType += 'Promise'
+            break;
+          };
         }
       }
-
-      if(!functionCatch&&!promiseCatch) return;
-      //  step2.inject report function
-      if(this.opts.import){ //  import declaration
+      if(functionCatch!==true&&promiseCatch!==true) return;
+      //  step2.判断上报方式
+      if(this.opts.import){ // 导入外部方法模式
         const uidName = file.path.scope.generateUidIdentifier('report');
         const {name,isDefault,source} = this.opts.import;
         if(!name||!source) throw new Error('need  name and source options!');
-        if(isDefault===true){ // importDefaultSpecifier
+        if(isDefault===true){
           file.path.node.body.unshift(
             t.importDeclaration(
               [t.importDefaultSpecifier(uidName)],
@@ -164,12 +171,13 @@ module.exports = function () {
               t.stringLiteral(source)))
         }
         reportFn = uidName;
-      }else{ // global declaration
+      }else{ // 全局声明模式
         if(!this.opts.reportFn) throw new Error('need a report function name:string or import options:object');
         reportFn = t.identifier(this.opts.reportFn);
       }
-      console.log('*********************************try-catching file : '+fileName);
+      console.log('\n')
+      console.log('\033[32m try-catching file : \033[0m','\033[33m '+fileName+' \033[0m','\033[32m   catchType : \033[0m','\033[36m '+catchType+' \033[0m');
       file.path.traverse(funcVisitor); // enhance before other plugins
-    }
+    },
   };
 };
